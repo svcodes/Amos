4832d27515be57533c325e4cd46a5d0dd724a5c5
svcodes <61701181+svcodes@users.noreply.github.com>
2021-02-26 15:37:39 -0600

    Update fun.py

--- cogs/fun/fun.py
+++ cogs/fun/fun.py
@@ -1,1 +1,22 @@
+from discord import Embed
+from discord.ext import commands
 
+from templatebot import Bot
+
+class Fun(commands.Cog):
+    """A set of fun commands for Amos"""
+
+    def __init__(self, bot: Bot):
+        self.bot = bot
+
+    @commands.command(name="duckgen")
+    async def duckgen(self, ctx: commands.Context, duckname = None):
+      name = duckname or ctx.author.name
+      embed = Embed(title="Duck!", description=f"Here's your duck, {ctx.author.mention}!")
+      embed.set_image(f"https://ducks.vcokltf.re/duck/{name}")
+      await ctx.send(embed=embed)
+    
+
+
+def setup(bot: Bot):
+    bot.add_cog(Fun(bot))
